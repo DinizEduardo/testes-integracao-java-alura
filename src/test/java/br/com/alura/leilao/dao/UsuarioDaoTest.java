6c03ff616e4d47eb8abed52f9008e13b77d6f11d
DinizEduardo <eduardo.diniz987@gmail.com>
2021-02-22 11:53:14 -0300

    Testando se ele não achará com username que não existe

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -3,9 +3,10 @@ package br.com.alura.leilao.dao;
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.utils.JPAUtils;
 import org.junit.Test;
-import org.springframework.util.Assert;
+import org.junit.Assert;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +15,7 @@ public class UsuarioDaoTest {
     private UsuarioDao dao;
 
     @Test
-    public void testeBuscaDeUsuarioPeloUsername() {
+    public void deveriaAcharUsuarioPeloUsername() {
 
         EntityManager em = JPAUtils.getEntityManager();
 
@@ -28,7 +29,24 @@ public class UsuarioDaoTest {
 
         Usuario procurado = this.dao.buscarPorUsername(usuario.getNome());
 
-        Assert.notNull(procurado, "Usuario deve ser encontrado!");
+        Assert.assertNotNull(procurado);
+
+    }
+
+    @Test
+    public void naoDeveriaAcharUsuarioPeloUsername() {
+
+        EntityManager em = JPAUtils.getEntityManager();
+
+        this.dao = new UsuarioDao(em);
+
+        Usuario usuario = new Usuario("fulano", "fulano@email.com", "123456");
+
+        em.getTransaction().begin();
+        em.persist(usuario);
+        em.getTransaction().commit();
+
+        Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("Beltrano"));
 
     }
 
